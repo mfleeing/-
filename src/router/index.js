363892a31357fac4c6b9e5d95510b2af281bee3a
mfleeing <1769240409@qq.com>
2020-02-03 21:51:47 +0800

    这是我的测试网页

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,8 @@ Vue.use(VueRouter)
 
 const routes = [{
         path: '/',
-        name: 'home',
-        component: Home,
+        name: 'api',
+        component: api,
         children: [{
                 path: 'play',
                 name: 'play',
